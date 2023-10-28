87cf473ef35b23195169f29a0e356732fb0fc9c8
alireza-maleki14 <148278866+alireza-maleki14@users.noreply.github.com>
2023-10-28 22:34:42 +0330

    Update Assignment4_2.py

--- Assignment4_2.py
+++ Assignment4_2.py
@@ -4,20 +4,8 @@ count=0
 while True:
 
     count+=1
-    if tas==1:
-        print("1")
-        break
-    elif tas==2:
-        print("2")
-        break
-    elif tas==3:
-        print("3")
-        break
-    elif tas==4:
-        print("4")
-        break
-    elif tas==5:
-        print("5")
+    if tas==1 or tas==2 or tas==3 or tas==4 or tas==5:
+        print(tas)
         break
     elif tas==6:
         print("6")
